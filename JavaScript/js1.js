127757108031e75f98c7ad0c2dcad6acc17a03d5
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-10-03 15:20:26 +0530

    num and math  function

---
+++ JavaScript/js1.js
@@ -0,0 +1,6 @@
+let name = "kaleem khan "
+let anothername = name + "Akhtar"; 
+console.log(anothername)
+// modern use of template laterels in java script 
+console.log(`myname is ${name} and my full name is ${anothername} `)
+console.log(name.toUpperCase())
---
+++ JavaScript/num&math.js
@@ -0,0 +1,4 @@
+// print the number using Math randome function between 10 to 20 
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random()*(max - min +1)) +min);
