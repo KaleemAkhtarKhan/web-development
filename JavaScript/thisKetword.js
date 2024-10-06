26499b37ab00fd58d1507c76286c53f717ca1ea8
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-10-06 13:07:44 +0530

    JAVASCRIPT PRACTICE

---
+++ JavaScript/thisKetword.js
@@ -0,0 +1,12 @@
+const user = {
+    username : "kaleem khan",
+    Qulification : "B. tech ",
+    MobiliNo : 9336700826 ,
+    wlecomeMsg() {
+        console.log(`${this.username} , welcome to this website `);
+    }
+}
+// console.log(user)
+user.wlecomeMsg()
+username: "AKHTAR"
+user.wlecomeMsg()
